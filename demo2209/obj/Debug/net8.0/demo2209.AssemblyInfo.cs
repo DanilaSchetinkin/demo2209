1900191d40bb5838590a8acee3317d63663b0183
DanilaSchetinkin <danya.sch2015@gmail.com>
2025-11-04 22:18:56 +0300

    -vibe

--- demo2209/obj/Debug/net8.0/demo2209.AssemblyInfo.cs
+++ demo2209/obj/Debug/net8.0/demo2209.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demo2209")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71a603a35222238044a462c44197837666ed8854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+209bc941259c09ad55d3fbd36fec758552b39510")]
 [assembly: System.Reflection.AssemblyProductAttribute("demo2209")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demo2209")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
